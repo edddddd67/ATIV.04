9fce2242c3be8c7e49e5c97f53cb8e33185b8154
edddddd67 <edjuniormendes74@email.com>
2024-09-09 08:45:45 -0300

    atividade 4

---
+++ ativ04.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+n1 = float(input("nota1"))
+n2 = float(input("nota2"))
+n3 = float(input("nota3"))
+soma = (n1+n2+n3)/3
+print (soma)
